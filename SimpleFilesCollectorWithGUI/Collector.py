f9b13ae0d24aef2c8f652d673f045ea3352ed446
RomanKlimov <itis.klimov@gmail.com>
2017-10-07 13:46:30 +0300

    fixed

--- SimpleFilesCollectorWithGUI/Collector.py
+++ SimpleFilesCollectorWithGUI/Collector.py
@@ -8,7 +8,6 @@ root = Tk()
 
 root.geometry('310x310')
 root.title = ('smth')
-#canvas = Canvas(root, width=310, height=310, bg='#002')
 label_cp = Label(root, text='Enter path for collecting: ')
 label_cp.place(x=0, y=10)
 label_tof = Label(root, text='Enter type of file: ')
@@ -27,8 +26,6 @@ btn_collect = Button(root, text='Collect')
 btn_collect.bind('<Button-1>', lambda event: collect(entry_cp.get(), entry_tof.get(), entry_sf.get()))
 btn_collect.place(x=150, y=70)
 
-root.mainloop()
-
 def collect(collecting_path, type_of_file, saving_folder):
     if not os.path.isdir(saving_folder):
         os.makedirs(saving_folder)
@@ -41,24 +38,4 @@ def collect(collecting_path, type_of_file, saving_folder):
             if name in list_of_types:
                 shutil.copy(path + '\\' + file, saving_folder)
 
-
-
-
-#collecting_path = input('Enter path for collecting: ')
-# list_of_types = []
-# while type_of_file != 'stop':
-#     type_of_file = input('Enter type of file or \'stop\': ')
-#     list_of_types.append(type_of_file)
-
-# def collect(collecting_path, type_of_file, saving_folder):
-#     if not os.path.isdir(saving_folder):
-#         os.makedirs(saving_folder)
-#     list_of_types = [type_of_file]
-#
-#     for path, package, files in os.walk(top=collecting_path):
-#         for file in files:
-#             index = file.rindex('.')
-#             name = file[index+1:]
-#             if name in list_of_types:
-#                 shutil.copy(path + '\\' + file, saving_folder)
-
+root.mainloop()
